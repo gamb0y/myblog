7a4eb50e3f54fc70406031f3746f0098a46376cd
gamb0y <64239413+gamb0y@users.noreply.github.com>
2020-07-08 13:39:23 +0900

    modified using db

--- app.js
+++ app.js
@@ -3,7 +3,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const _ = require('lodash');
+const _ = require("lodash");
+const mongoose = require("mongoose");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -16,53 +17,77 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-let posts = [];
+const url = 'mongodb://localhost:27017/todolistDB';
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 
-app.get('/', function(req, res) {
-  res.render('home', {homeStartingContent: homeStartingContent, posts: posts});
+const blogSchema = new mongoose.Schema({
+  title: String,
+  content: String
 });
 
-app.get('/posts/:article', function(req, res) {
-  // console.log(req.params);
-  let postTitle = _.lowerCase(req.params.article);
+const Blog = mongoose.model('Blog', blogSchema);
 
-  posts.forEach( function(element) {
-    let title = _.lowerCase(element.title);
 
-    if ( title === postTitle ) {
-      res.render('post', {postTitle: element.title, postContent: element.content});
+app.get("/", function(req, res){
+
+  Blog.find({}, function(err, posts) {
+
+    if (!err) {
+      res.render("home", { startingContent: homeStartingContent, posts: posts });
+    } else {
+      res.redirect("/");
     }
   });
 });
 
-app.get('/about', function(req, res) {
-  res.render('about', {aboutContent: aboutContent});
+
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
 });
 
-app.get('/contact', function(req, res) {
-  res.render('contact', {contactContent: contactContent});
+
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
 });
+
 
 app.get('/count', function(req, res) {
   res.render('count');
 });
 
-app.get('/compose', function(req, res) {
-  res.render('compose');
+
+app.get("/compose", function(req, res){
+  res.render("compose");
 });
 
-app.post('/compose', function(req, res) {
-  const postArticle = {
+
+app.post("/compose", function(req, res){
+
+  const post = new Blog({
     title: req.body.postTitle,
-    content: req.body.postContent
-  };
+    content: req.body.postBody
+  });
 
-  posts.push(postArticle);
+  post.save(function(err) {
+    if (err) {
+      console.log(err);
+    }
+  });
 
-  res.redirect('/');
+  res.redirect("/");
 });
 
 
+app.get("/posts/:postId", function(req, res){
+
+  Blog.findById(req.params.postId, function(err, post) {
+    if (!err) {
+      res.render("post", { title: post.title, content: post.content });
+    } else {
+      res.redirect("/");
+    }
+  });
+});
 
 
 
